0349e3dad949a96855a918069cc36c061731f7d9
claire froelich <claire.froelich@gmail.com>
2020-05-27 14:46:15 -0400

    buggy drag n drop

--- src/components/memeSVG.js
+++ src/components/memeSVG.js
@@ -2,6 +2,26 @@ import React, {useState, useEffect} from 'react';
 
 const MemeSVG = ({ data, text }) => {
   const aspectRatio = data.width/data.height;
+  const [topY, setTopY] = useState(200)
+  const [topX, setTopX] = useState(100)
+
+  const trackTop = (e) => {
+    setTopX(e.offsetX)
+    setTopY(e.offsetY)
+  }
+
+  const handleMouseDown = (e, pos) => {
+    if(pos === 'top') {
+      e.target.addEventListener('mousemove', trackTop)
+    }
+  }
+
+  const handleMouseUp = (e, pos) => {
+    if(pos === 'top') {
+      e.target.removeEventListener('mousemove', trackTop)
+    }
+  }
+
 
   return (
 
@@ -10,12 +30,12 @@ const MemeSVG = ({ data, text }) => {
     height={700}
     >
       <image xlinkHref={data.url} width={700} height={700*aspectRatio} />
-      <text x={100} y={200}> {text} </text>
+      <text x={topX} y={topY} onMouseDown={e => handleMouseDown(e, 'top')} onMouseUp={e => handleMouseUp(e, 'top')}> {text} </text>
     </svg>
   )
 }
 
 export default MemeSVG;
 
-// <div className="meme-image" style={{backgroundImage: `url(${url})`}}>
+// <div className="meme-image()" style={{backgroundImage: `url(${url})`}}>
 // </div>
