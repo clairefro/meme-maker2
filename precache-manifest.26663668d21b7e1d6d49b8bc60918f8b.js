633b2bd24cf80f8fb135309d36ad8e4b39a4f672
claire froelich <claire.froelich@gmail.com>
2020-05-27 14:07:37 -0400

    Updates

---
+++ precache-manifest.26663668d21b7e1d6d49b8bc60918f8b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a28ed870a9eb7940e0812a53a260d4b1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "140c09fc34a2b8db02de",
+    "url": "/static/css/main.1cde7a8a.chunk.css"
+  },
+  {
+    "revision": "69cded44559c65a1968b",
+    "url": "/static/js/2.41c17770.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.41c17770.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "140c09fc34a2b8db02de",
+    "url": "/static/js/main.4d4380ae.chunk.js"
+  },
+  {
+    "revision": "1b142a450f984cc6cba1",
+    "url": "/static/js/runtime-main.25f92e39.js"
+  }
+]);
